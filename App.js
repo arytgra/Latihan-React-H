1794e18fadce1574a4c3bab246d4ba066d0ba888
arytgra <88172851+arytgra@users.noreply.github.com>
2022-03-13 22:37:30 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h3 className="judul">Ini Project Pertama React JS</h3>
+    <p className="subJudul">Belajar React JS itu mudah</p>
+
+
+    </div>
+  );
+}
+
+export default App;
